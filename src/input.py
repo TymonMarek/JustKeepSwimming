d26494d8b021ddb5c9e8e3de85c167d1b8754fd3
Tymon Marek <tymon.marek@ulur.org>
2025-11-04 13:24:29 +0000

    Fixed input bug from trying to normalize a zero vector

--- src/input.py
+++ src/input.py
@@ -136,7 +136,7 @@ class Mouse:
     def on_mouse_moved(self, tick: Tick, event: pygame.event.Event) -> None:
         self.delta = pygame.Vector2(pygame.mouse.get_rel())
         self.position = pygame.Vector2(pygame.mouse.get_pos())
-        self.direction = pygame.Vector2(pygame.mouse.get_rel()).normalize()
+        self.direction = self.delta.normalize() if self.delta.magnitude() > 0 else pygame.Vector2(0, 0)
 
 
 class Input:
