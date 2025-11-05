0fec2176b11973ab3fb5d24bc81f3ef5d60e7d77
Tymon Marek <tymon.marek@ulur.org>
2025-11-05 14:36:07 +0000

    Changed Entity physics behavior to now be based off of efficiency and not friction

--- src/entities.py
+++ src/entities.py
@@ -5,26 +5,30 @@ from sprites import Sprite
 
 all: List["Entity"] = []
 
+def clamp(n: float, min_value: float, max_value: float) -> float:
+    return max(min_value, min(n, max_value))
+
 class Entity:
     def __init__(self) -> None:
         self.sprite = Sprite()
 
-        self.max_velocity = Vector2(200, 200)
+        self.max_velocity = Vector2(1500, 1500)
         self.acceleration: Vector2 = Vector2()
-        self.friction: float = 0.00
         self.velocity: Vector2 = Vector2()
         
         self.wish_direction: Vector2 = Vector2()
-        self.force = Vector2(5, 5)
+        self.force = Vector2(2, 2)
+        self.efficiency = 0.995
 
         self.__post_init__()
 
     def __post_init__(self) -> None:
         all.append(self)
-
+    
     def update(self, tick: Tick):
-        wish_direction = self.wish_direction.normalize() if self.wish_direction.magnitude() > 0 else Vector2(0, 0)
-        self.acceleration = Vector2(wish_direction.x * self.force.x, wish_direction.y * self.force.y)
-        self.velocity = self.acceleration
-        self.velocity = self.velocity * (1 - self.friction)
-        self.sprite.position = (self.sprite.position + self.velocity) * tick.delta_time
+        self.acceleration = Vector2(self.wish_direction.x * self.force.x, self.wish_direction.y * self.force.y)
+        self.velocity += self.acceleration
+        self.velocity = self.velocity * self.efficiency
+        self.velocity = Vector2(clamp(self.velocity.x, -self.max_velocity.x, self.max_velocity.x), clamp(self.velocity.y, -self.max_velocity.y, self.max_velocity.y))
+        self.sprite.position = self.sprite.position + (self.velocity * tick.delta_time)
+        print(self.acceleration, self.velocity, self.sprite.position)
