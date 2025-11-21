0549b41f4980bcf35fdcc59f4b0e566595ce868e
TymonMarek <tymon.marek@icloud.com>
2025-11-21 11:02:09 +0000

    Made physics stateful

--- src/physics.py
+++ src/physics.py
@@ -4,6 +4,11 @@ import entities
 class Physics:
     """A handler for all entities that support physics.
     """
+    def __init__(self) -> None:
+        self.enabled = False
+   
     async def update(self, tick: Tick):
+        if not self.enabled:
+            return
         for entity in entities.all:
             entity.update(tick)
